f8448d36c251ecc0a9d73981891dfdc4ef88291a
HarishanakarRU <harishankarr@berkeley.edu>
2026-03-17 17:11:32 -0700

    Checked for NaN values and examined column types

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,5 +1,22 @@
 import pandas as pd
 
-csv = pd.read_csv("netflix1.csv")
-print(csv.head())
-print(csv.columns)
+netflix = pd.read_csv("netflix1.csv")
+print(netflix.head())
+print(netflix.columns)
+print(netflix["rating"])
+
+# No missing values each column
+print(sum([sum(netflix[column].isna()) for column in netflix.columns])) # Equals 0
+
+# Examine Column Types of Dataset
+column_types = {}
+
+for column in netflix.columns:
+    column_types[column] = str(netflix[column].dtype)
+print(column_types)
+
+'''
+
+'''
+
+
